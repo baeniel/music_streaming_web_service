5ac03b820c9515ab013f0f2793c723e3fb033392
baeniel <jinveloper126@gmail.com>
2022-03-01 12:15:02 +0900

    재생목록 추가/삭제 사이드킥 제거

--- app/controllers/user_musics_controller.rb
+++ app/controllers/user_musics_controller.rb
@@ -9,26 +9,17 @@ class UserMusicsController < ApplicationController
   # 재생목록 추가
   def create
     @sort_condition = params[:sort_condition]
-    # n개 추가
-    if @music_ids.length > 1
-      AddPlaylistRecordsJob.perform_async(@music_ids, current_user.email)
-    # 1개 추가
-    else
-      music = Music.find @music_ids.join(",")
+    @music_ids.each do |music_id|
+      music = Music.find music_id
       current_user.user_musics.create(music: music)
-      CheckPlaylistRecordsJob.perform_async(current_user.email)
     end
   end
 
   # 재생목록 삭제
   def destroy
-    # n개 삭제
-    if @music_ids.length > 1
-      DestroyPlaylistRecordsJob.perform_async(@music_ids, current_user.email)
-    # 1개 삭제
-    else
-      music = Music.find @music_ids.join(",")
-      current_user.user_musics.where(music: music).order(created_at: :desc).first.destroy
+    @music_ids.each do |music_id|
+      music = Music.find music_id
+      current_user.user_musics.where(music: music).order(created_at: :desc).destroy_all
     end
   end
 
--- app/models/user_music.rb
+++ app/models/user_music.rb
@@ -1,4 +1,15 @@
 class UserMusic < ApplicationRecord
   belongs_to :user
   belongs_to :music, optional: true
+
+  after_save :check_record_count
+
+  private
+
+  def check_record_count
+    playlist_records = self.user.user_musics
+    if playlist_records.count > 100
+      playlist_records.limit(playlist_records.count - 100).order(:created_at).destroy_all
+    end
+  end
 end
